15a42a047de2ba680a2fba7aab045c7462859c7a
Chilaka Michael Obinna <obichilaka@gmailcom>
2024-03-26 05:02:39 +0100

    created a barebones of Layout.jsx, Header.jsx, Sidebar.jsx, Overview.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import OverView from '../pages/Overview';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 const Layout = () => {
   return (
-    <div>Transmonitor Layout</div>
-  )
-}
+    <div className='overflow-hidden h-screen'>
+      <Header />
 
-export default Layout
+      <main className='flex h-screen w-full'>
+        <div className='hidden md:block w-[260px] h-full bg-white !scrollbar-thin !scrollbar-track-transparent !scrollbar-thumb-grey'>
+          <Sidebar />
+        </div>
+
+        <section className='w-full md:w-[calc(100vw - 260px)] h-full !scrollbar-thin !scrollbar-track-transparent !scrollbar-thumb-grey pb-12 bg-outletBackground'>
+          {' '}
+          <OverView />
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
